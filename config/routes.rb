6d40bd7a33b354cdf81daf8d827830a36d64cbc3
buffalorob <peckham.rob@gmail.com>
2019-02-11 11:45:43 -0500

    namespaced routes under api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :items
-  use_doorkeeper
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :items
+      use_doorkeeper
+    end
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
