7f31f0b8f59660c37fe08f3d64aa4ede4df1d55c
buffalorob <peckham.rob@gmail.com>
2019-02-12 15:03:09 -0500

    changed routes to match example

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :items
-  devise_for :users
   use_doorkeeper
+
+  resources :items
+
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
+  root to: "home#index"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
