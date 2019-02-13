aa57107cd4783ed9f673ea9d075a00b3e4e28f5a
buffalorob <peckham.rob@gmail.com>
2019-02-13 13:28:32 -0500

    routes: + settings for doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # Use for login and autorize all resource
+  use_doorkeeper do
+    # No need to register client application
+    skip_controllers :applications, :authorized_applications
+  end
+
   scope module: :api, defaults: { format: :json }, path: 'api' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       devise_for :users, controllers: {
