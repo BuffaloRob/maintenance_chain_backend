66f1c3c5c02635c4fb98f6d73482a82129975bdc
buffalorob <peckham.rob@gmail.com>
2019-02-13 13:47:28 -0500

    routes: commented out constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # Use for login and autorize all resource
+  # Use for login and to authorize all resource
   use_doorkeeper do
     # No need to register client application
     skip_controllers :applications, :authorized_applications
   end
 
   scope module: :api, defaults: { format: :json }, path: 'api' do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    scope module: :v1 do #, constraints: ApiConstraints.new(version: 1, default: true) do
       devise_for :users, controllers: {
            registrations: 'api/v1/users/registrations',
        }, skip: [:sessions, :password]
